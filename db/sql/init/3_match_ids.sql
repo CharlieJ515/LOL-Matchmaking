0d0a53e7cb8d9474b9744d18fd4a472bc81b5a46
KyuSang Jang <charlie.jang515@gmail.com>
2025-08-02 12:41:59 +0900

    storage optimized via column organization and added normalized tables

---
+++ db/sql/init/3_match_ids.sql
@@ -0,0 +1,9 @@
+CREATE TABLE match_ids (
+    lease_until timestamptz NOT NULL DEFAULT 'epoch' :: timestamptz,
+    queried bool NOT NULL DEFAULT false,
+    match_id TEXT PRIMARY KEY,
+    region_name TEXT NOT NULL,
+    FOREIGN KEY (region_name) REFERENCES regions(region_name)
+);
+
+CREATE INDEX idx_match_ids_region_queried_lease_until_match_id ON match_ids (region, queried, lease_until, match_id);
---
+++ db/sql/init/5_perks.sql
@@ -0,0 +1,7 @@
+CREATE TABLE perks (
+    perk_id SMALLINT PRIMARY KEY,
+    perk_name TEXT NOT NULL,
+    var1_desc TEXT,
+    var2_desc TEXT,
+    var3_desc TEXT
+);
