f81f13befee636c13f31f7da8d5d4409f71c3137
KyuSang Jang <charlie.jang515@gmail.com>
2025-08-11 11:05:02 +0900

    modified index based on query plan to use index instead of sequential scan

--- db/sql/init/2_users.sql
+++ db/sql/init/2_users.sql
@@ -6,9 +6,21 @@ CREATE TABLE users (
 	FOREIGN KEY (platform_name) REFERENCES platforms(platform_name)
 );
 
-CREATE INDEX idx_platform_match_id_queried_lease_until_puuid ON users (
-	platform_name,
-	match_id_queried,
-	lease_until,
-	puuid
-);
+--CREATE INDEX idx_platform_match_id_queried_lease_until_puuid ON users (
+--	platform_name,
+--	match_id_queried,
+--	lease_until,
+--	puuid
+--);
+
+--create index idx_users_platform_lease_until_queried__incl_puuid on users (
+--	platform_name,
+--	lease_until,
+--	match_id_queried
+--) include (puuid);   
+
+CREATE INDEX IF NOT EXISTS idx_users_platform_lease
+  ON users (platform_name, lease_until);
+
+CREATE INDEX IF NOT EXISTS idx_users_platform_matchq
+  ON users (platform_name, match_id_queried);
